5c8cdd8e3d06fb728c9ba0e5620f933cbb7f8302
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-21 18:19:53 -0400

    solution

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import itemData from '../data/items'
 
-function ShoppingList({ items }) {
+function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState(itemData)
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  const onItemFormSubmit = (newItem) => {
+    setItems([...items, newItem])
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -18,7 +24,7 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
       <Filter onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
